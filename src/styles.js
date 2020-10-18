f6b64643f924ce8f8aa207d04034c7bafcb717f7
Nurettin Güngör <gungorn@outlook.com>
2020-10-19 00:43:08 +0300

    anasayfa tasarımı

--- src/styles.js
+++ src/styles.js
@@ -73,3 +73,51 @@ export const GirisYapS = StyleSheet.create({
 export const UyeOlS = StyleSheet.create({
     ...GirisYapS, //stiller tamamane aynı olduğu için, spread operatörü ile birleştirdim
 });
+
+
+export const AnasayfaS = StyleSheet.create({
+    container: {
+        flex: 1,
+        backgroundColor: '#fff'
+    },
+    background: {
+        position: 'absolute',
+        top: 0,
+        left: 0
+    },
+
+    topContainer: {
+        flexDirection: 'row',
+        width: w,
+        paddingHorizontal: W(2),
+        paddingVertical: W(2),
+        alignItems: 'center',
+    },
+    avatar: {
+        borderRadius: 999,
+        aspectRatio: 1
+    },
+    name: {
+        fontSize: 20,
+        fontWeight: 'bold',
+        flex: 1,
+        color: '#fff',
+        marginLeft: W(3)
+    },
+    searchInputContainer: {
+        backgroundColor: '#ffffff33',
+        borderRadius: 999,
+        marginHorizontal: W(10),
+        flexDirection: 'row',
+        paddingHorizontal: 10,
+        paddingLeft: W(5),
+        paddingVertical: 10
+    },
+    searchInput: {
+        padding: 0,
+        marginLeft: W(3),
+        flex: 1,
+        color: '#fff',
+        fontSize: 16
+    }
+});
--- src/views/Anasayfa.js
+++ src/views/Anasayfa.js
@@ -1,11 +1,53 @@
 import React from 'react';
-import { View, Text } from 'react-native';
+import { View, Text, Image, StatusBar, TouchableOpacity, TextInput } from 'react-native';
+import SImage from 'react-native-scalable-image';
+import { getStatusBarHeight as sbh } from 'react-native-status-bar-height';
+import Entypo from 'react-native-vector-icons/Entypo';
+import FA from 'react-native-vector-icons/FontAwesome';
+
 import UyelikM from '../models/UyelikM';
+import { AnasayfaS as S, W } from '../styles';
 
 export default () => {
     return (
-        <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-            <Text>ANASAYFA, {UyelikM.uid}</Text>
+        <View style={S.container}>
+            <StatusBar barStyle={'light-content'} translucent backgroundColor={'transparent'} />
+
+            <SImage
+                source={require('../../assets/b1.png')}
+                style={S.background}
+                width={W(100)}
+                resizeMode={'contain'}
+            />
+
+            <View style={{ height: sbh() }} />
+
+            <View style={S.topContainer}>
+                <SImage
+                    source={{ uri: 'https://www.w3schools.com/howto/img_avatar.png' }}
+                    style={S.avatar}
+                    width={W(13)}
+                />
+
+                <Text style={S.name}>Nurettin Güngör</Text>
+
+                <TouchableOpacity>
+                    <Entypo name={'game-controller'} color={'#fff'} size={32} />
+                </TouchableOpacity>
+                <TouchableOpacity>
+                    <Entypo name={'game-controller'} color={'#fff'} size={32} />
+                </TouchableOpacity>
+                <TouchableOpacity>
+                    <Entypo name={'game-controller'} color={'#fff'} size={32} />
+                </TouchableOpacity>
+            </View>
+
+            <View style={S.searchInputContainer}>
+                <FA name={'search'} color={'#fff'} size={28} />
+                <TextInput
+                    style={S.searchInput}
+                />
+            </View>
         </View>
     );
 }
